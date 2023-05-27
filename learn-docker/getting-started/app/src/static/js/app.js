7863436d542656aced0e3aa3ba81b98ac957a9aa
hixcoder <hixcoder@gmail.com>
2023-05-27 14:42:38 +0100

    finish docker get-started

--- learn-docker/getting-started/app/src/static/js/app.js
+++ learn-docker/getting-started/app/src/static/js/app.js
@@ -53,7 +53,7 @@ function TodoListCard() {
         <React.Fragment>
             <AddItemForm onNewItem={onNewItem} />
             {items.length === 0 && (
-                <p className="text-center">No items yet! Add one above!</p>
+                <p className="text-center">No items yet! zid chi 7aja!</p>
             )}
             {items.map(item => (
                 <ItemDisplay

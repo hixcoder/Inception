5d44ca2408f7741ea741a97c48b3efaeb7d9c1e4
hixcoder <hixcoder@gmail.com>
2023-06-05 12:05:31 +0100

    finish mariadb container

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -2,15 +2,15 @@
 
 service mysql start 
 
-
-echo "CREATE DATABASE IF NOT EXISTS $myDb_name ;" > myDb.sql
-echo "CREATE USER IF NOT EXISTS '$myDb_user'@'%' IDENTIFIED BY '$myDb_pwd' ;" >> myDb.sql
-echo "GRANT ALL PRIVILEGES ON $myDb_name.* TO '$myDb_user'@'%' ;" >> myDb.sql
-echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '12345' ;" >> myDb.sql
+echo "CREATE DATABASE IF NOT EXISTS wordpress;" > myDb.sql
+echo "CREATE USER IF NOT EXISTS 'hboumahd'@'%' IDENTIFIED BY 'hix1234';" >> myDb.sql
+echo "GRANT ALL PRIVILEGES ON wordpress.* TO 'hboumahd'@'%';" >> myDb.sql
+echo "ALTER USER 'root'@'localhost' IDENTIFIED BY 'hix0000';" >> myDb.sql
 echo "FLUSH PRIVILEGES;" >> myDb.sql
 
 mysql < myDb.sql
 
-# kill $(cat /var/run/mysqld/mysqld.pid)
+kill $(cat /var/run/mysqld/mysqld.pid)
 
 mysqld
+

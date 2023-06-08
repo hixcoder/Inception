528f1b67edd3c9a524b993c487a0f0e0be11a9b0
hixcoder <hixcoder@gmail.com>
2023-06-08 15:13:49 +0100

    trying to connect mariadb and wordpress containers

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -2,12 +2,12 @@
 
 db_name="wordpress"
 db_user="hboumahd"
-db_pwd="hix1234"
+db_pass="hix1234"
 
 service mysql start 
 
 echo "CREATE DATABASE IF NOT EXISTS $db_name;" > myDb.sql
-echo "CREATE USER IF NOT EXISTS '$db_user'@'%' IDENTIFIED BY '$db_pwd';" >> myDb.sql
+echo "CREATE USER IF NOT EXISTS '$db_user'@'%' IDENTIFIED BY '$db_pass';" >> myDb.sql
 echo "GRANT ALL PRIVILEGES ON $db_name.* TO '$db_user'@'%';" >> myDb.sql
 echo "ALTER USER 'root'@'localhost' IDENTIFIED BY 'root0000';" >> myDb.sql
 echo "FLUSH PRIVILEGES;" >> myDb.sql
@@ -16,4 +16,4 @@ mysql < myDb.sql
 
 kill $(cat /var/run/mysqld/mysqld.pid)
 
-mysqld
+# mysqld
--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -1,54 +1,43 @@
 #!/bin/bash
 
+db_name="wordpress"
+db_user="hboumahd"
+db_pass="hix1234"
+
+DOMAIN_NAME="klj.com"
+WP_TITLE="hixwordpress"
+WP_ADMIN_USR="hixroot"
+WP_ADMIN_PWD="hix1234root"
+WP_ADMIN_EMAIL="hix.root@gmail.com"
+WP_USR="hix"
+WP_EMAIL="hix.user@gmail.com"
+WP_PWD="hix1234user"
+
+
 mkdir /var/www/
+
+# For install wordpress
 mkdir /var/www/html
-
 cd /var/www/html
-
-
 rm -rf *
-
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar 
-
 chmod +x wp-cli.phar 
-
 mv wp-cli.phar /usr/local/bin/wp
-
-
 wp core download --allow-root
 
+# update the wordpress config file(wp-config.php)
 mv /var/www/html/wp-config-sample.php /var/www/html/wp-config.php
-
 mv /wp-config.php /var/www/html/wp-config.php
-
-
-sed -i -r "s/db1/$db_name/1"   wp-config.php
+sed -i -r "s/myDb/$db_name/1"  wp-config.php
 sed -i -r "s/user/$db_user/1"  wp-config.php
-sed -i -r "s/pwd/$db_pwd/1"    wp-config.php
+sed -i -r "s/pass/$db_pass/1"  wp-config.php
 
+
+# For setup wordpress
 wp core install --url=$DOMAIN_NAME/ --title=$WP_TITLE --admin_user=$WP_ADMIN_USR --admin_password=$WP_ADMIN_PWD --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root
-
-
-
-
 wp user create $WP_USR $WP_EMAIL --role=author --user_pass=$WP_PWD --allow-root
 
-
-wp theme install astra --activate --allow-root
-
-
-wp plugin install redis-cache --activate --allow-root
-
-wp plugin update --all --allow-root
-
-
- 
-sed -i 's/listen = \/run\/php\/php7.4-fpm.sock/listen = 9000/g' /etc/php/7.4/fpm/pool.d/www.conf
-
-mkdir /run/php
-
-
-
-wp redis enable --allow-root
-
-/usr/sbin/php-fpm7.4 -F
+# For configure and run PHP-FPM
+# sed -i 's/listen = \/run\/php\/php7.4-fpm.sock/listen = 9000/g' /etc/php/7.4/fpm/pool.d/www.conf
+# mkdir /run/php
+# /usr/sbin/php-fpm7.4 -F
